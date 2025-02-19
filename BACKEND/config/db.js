2a6db86e1e3131bd043b3ae3175d7fa5dbac4230
Arnold Tony <arnoldtony269@gmail.com>
2025-02-19 10:33:57 +0300

    changed config on db

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected');
-  } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
     process.exit(1);
   }
 };
